f21485e59316c15b68568f3a85154b0e736db7d7
d1ksu <diksu.kontakt@gmail.com>
2022-07-16 22:54:07 +0200

    events

--- src/main/java/me/d1ksu/onehard/megadrop/event/GuildDeleteEvent.java
+++ src/main/java/me/d1ksu/onehard/megadrop/event/GuildDeleteEvent.java
@@ -1,8 +1,36 @@
 package me.d1ksu.onehard.megadrop.event;
 
+import me.d1ksu.onehard.megadrop.guild.Guild;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
 /**
  * @author d1ksu
  * @Date 15.07.2022
  */
-public class GuildDeleteEvent {
+public class GuildDeleteEvent extends Event {
+
+    private static final HandlerList handlerList = new HandlerList();
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+
+    private final Guild guild;
+
+    public GuildDeleteEvent(Guild guild) {
+        this.guild = guild;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    public Guild getGuild() {
+        return guild;
+    }
+
+
 }
--- src/main/java/me/d1ksu/onehard/megadrop/event/GuildMemberJoinEvent.java
+++ src/main/java/me/d1ksu/onehard/megadrop/event/GuildMemberJoinEvent.java
@@ -1,8 +1,41 @@
 package me.d1ksu.onehard.megadrop.event;
 
+import me.d1ksu.onehard.megadrop.guild.Guild;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
 /**
  * @author d1ksu
  * @Date 15.07.2022
  */
-public class GuildMemberJoinEvent {
+public class GuildMemberJoinEvent extends Event {
+
+    private static final HandlerList handlerList = new HandlerList();
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+
+    private final Guild guild;
+    private final Player player;
+
+    public GuildMemberJoinEvent(Guild guild, Player player){
+        this.guild = guild;
+        this.player = player;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    public Guild getGuild() {
+        return guild;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }
